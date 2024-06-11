4805b8dd31f55ae6f41640b2b3330bcbaed9c490
Shivaprasad100 <145660804+Shivaprasad100@users.noreply.github.com>
2024-06-11 22:46:37 +0530

    Update snake.js
    Making more cleaner script

--- snake.js
+++ snake.js
@@ -42,7 +42,9 @@ let paused = false;
 
 window.addEventListener("keydown", changeDirection);
 
+//Adding Moving-Functoinalities
 function changeDirection(event){
+    //Keys are p=pause, upArrow, rightArrow, downArrow, leftArrow
     let keyPressed = event.keyCode;
 
     const upKey = 38;
@@ -51,31 +53,36 @@ function changeDirection(event){
     const leftKey = 37;
 
     
-    
+    //Snake on moving directions
     let goingRight = (xVelocity == unitSize);
     let goingDown = (yVelocity == unitSize);
     let goingLeft = (xVelocity == -unitSize);
     let goingUp = (yVelocity == -unitSize);
 
 
+    //To go upWard by upArtow key
     if(!goingUp && keyPressed == upKey){
         xVelocity = 0;
         yVelocity = -unitSize;
     }
+    //To go RightWard by DownWard arrow 
     else if(!goingRight && keyPressed == rightKey){
         xVelocity = unitSize;
         yVelocity = 0;
 
     }
+    //To go DownWard by DownAtrow
     else if(!goingDown && keyPressed == downKey){
         xVelocity = 0;
         yVelocity = unitSize;
     }
+    //To go Left
     else if(!goingLeft && keyPressed == leftKey){
         xVelocity = -unitSize;
         yVelocity = 0;
     }
 
+    //To pause game and Resume
     if(keyPressed == 80 && running === true){
         if(paused){
             paused = false;
@@ -86,10 +93,12 @@ function changeDirection(event){
         }
     }
 }
+
 resetBtn.addEventListener("click", resetGame);
 
 gameStart();
 
+//To start the Game
 function gameStart(){
 
     running = true;
@@ -100,7 +109,9 @@ function gameStart(){
 
 }
 
+//Next Move on Time
 function nextTick(){
+    
     if(running && !paused){
         setTimeout(()=>{
             clearBoard();
@@ -117,6 +128,7 @@ function nextTick(){
     }
 
 }
+
 function createFood(){
     function randomFood(min, max){
         let randNum = Math.round((Math.random()*(max-min) + min)/unitSize)*unitSize;
@@ -149,9 +161,9 @@ function moveSnake(){
     }
 
     snake.unshift(head);
-    //console.log(snake);
+    
 
-    //drawHead(headToStyle);
+    
     
     if(snake[0].x === foodX && snake[0].y === foodY){
         score++;
@@ -164,19 +176,15 @@ function moveSnake(){
     }
 };
 
+//draw head stroke to Red
 function drawHead(headToStyle){
-    //console.log(headToStyle);
-    ctx.fillStyle = "black";
     ctx.strokeStyle = "red";
-    ctx.fillRect(headToStyle.x, headToStyle, unitSize, unitSize);
     ctx.strokeRect(headToStyle.x, headToStyle.y, unitSize, unitSize);
     ctx.stroke();
-    ctx.fill()
-
 }
 
+//Drawing Snake
 function drawSnake(){
-
     ctx.fillStyle = snakeColor;
     ctx.strokeStyle = snakeBorder;
     snake.forEach(snakePart =>{
@@ -185,19 +193,20 @@ function drawSnake(){
     });
 
     drawHead(snake[0]);
-};
+}
+
 function checkGameOver(){
     if(snake[0].x < 0 || snake[0].y < 0 || snake[0].x >= gameWidth || snake[0].y >= gameHeight){
         running = false;
     }   
-};
+}
 
 function displayGameOver(){
     ctx.font = "50PX MV BOLI";
     ctx.fillStyle = "black";
     ctx.textAlign = "center";
     ctx.fillText("GAME OVER..", gameWidth/2, gameHeight/2);
-};
+}
 
 function resetGame(){
     if(highestScore < score){
